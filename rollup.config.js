dc837eb50021a50be9be6c689767865168a6b78f
RGBKnights <master@rgbknights.com>
2022-03-14 23:22:10 -0400

    default setup for 0.1.1-rc

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,33 @@
+"use strict";
+
 import clear from "rollup-plugin-clear";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
 
-function checkScriptLimit() {
-    const scriptLimit = 10000000; // 10mb
-    return {
-        generateBundle(_options, bundle) {
-            for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
-                if (fileName === "main.mjs" && chunkOrAsset.code && chunkOrAsset.code.length >= scriptLimit * 0.98) {
-                    console.log(
-                        `Warning: Script limit is ${scriptLimit / 1000000}mb, output is ${chunkOrAsset.code.length} bytes`
-                    );
-                }
-            }
-        }
-    };
+let externals = [
+  "game", 
+  "game/prototypes",
+  "game/constants", 
+  "game/utils", 
+  "game/path-finder", 
+  "game/visual", 
+  "arena"
+]
+
+export default {
+  input: "src/main.ts",
+  external: externals,
+  output: {
+    file: 'dist/main.mjs',
+    paths: (path) => {
+      return externals.includes(path) ? "/" + path : path;
+    }
+  },
+  plugins: [
+    clear({ targets: ["dist"]}),
+    resolve({ rootDir: "src"}),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" })
+  ]
 }
-
-module.exports = {
-    input: 'src/index.js',
-    external: ["game", "game/prototypes", "game/constants", "game/utils", "game/path-finder", "arena"], // <-- suppresses the warning
-    output: {
-        dir: "dist/",
-        format: 'esm',
-        entryFileNames: "main.mjs",
-        paths: path => {
-            if (path.startsWith("game") || path.startsWith("arena")) {
-                return "/" + path;
-            }
-        },
-    },
-    plugins: [
-        clear({ targets: ["dist"] }),
-        resolve({ rootDir: "src" }),
-        commonjs(),
-        checkScriptLimit(),
-    ],
-};
